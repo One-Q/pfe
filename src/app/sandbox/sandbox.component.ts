a8da29cf4a552faa26d6774be6b94c3de057b0a9
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2018-06-12 17:16:57 +0200

    Sandbox Select validators

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,18 +1,50 @@
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sandbox',  // <home></home>
   styleUrls: [ './sandbox.component.scss' ],
   templateUrl: './sandbox.component.html'
 })
-export class SandboxComponent {
+export class SandboxComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.buildForm()
+  }
 
   isVisible: boolean = true;
   isDisabled: boolean = false;
+  form: FormGroup
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
 
   }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: [''],
+      role: ['']
+    })
+  }
+
+  checkRole() {
+    let role = this.form.value.role
+    let passwordController = this.form.controls['password']
+    if (role == 'teacher') {
+      passwordController.setValidators(Validators.required)
+      passwordController.updateValueAndValidity()
+      return true
+    }
+    passwordController.clearValidators()
+    passwordController.updateValueAndValidity()
+    return false
+  }
+
+  submit() {
+    console.log(this.form.valid)
+  }
+
     
 }
